9982bbab29f8ad082f27fdac1d3ba528adb7d3c2
johnlocker <jonathan.klaiber@gmail.com>
2015-06-09 19:34:55 +0200

    infobox done, phyton api test file

---
+++ NPOFinder/test code/rest.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 09 17:12:28 2015
+
+@author: Besitzer
+"""
+
+import requests
+url = 'http://backstage-api.openstate.eu/v0/search'
+data = '{"query": "leenstelsel","facets": {"collection": {},"date": {"interval": "day"}},"filters": {"media_content_type": {"terms": ["image/jpeg"]}},"size": 1}'
+response = requests.post(url, data=data)
